4778eb2bc9184cbbb150de3edc8548a9e705c535
ivanm <matyivan@gmail.com>
2025-03-12 08:11:25 +0100

    http basic removed

--- src/main/java/com/project/pushup/security/SecurityConfig.java
+++ src/main/java/com/project/pushup/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         http
             .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(Customizer.withDefaults())
+//            .httpBasic(Customizer.withDefaults())
             //permit all
             .authorizeHttpRequests(auth -> auth
                 .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.ERROR).permitAll()
