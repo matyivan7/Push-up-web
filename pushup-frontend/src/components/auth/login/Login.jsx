b98b9a644f0eae375a191e208611f72f1efd7b8c
ivanm <matyivan@gmail.com>
2025-03-19 14:01:35 +0100

    URl

--- pushup-frontend/src/components/auth/login/Login.jsx
+++ pushup-frontend/src/components/auth/login/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
                         <input
                             type="text"
                             id="username"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            className="block py-2.5 px-2 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
@@ -46,7 +46,7 @@ const Login = () => {
                         <input
                             type="password"
                             id="login-password"
-                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                            className="block py-2.5 px-2 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                             placeholder=" "
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
@@ -61,7 +61,7 @@ const Login = () => {
                     </div>
                     <button
                         type="submit"
-                        className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                        className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 my-1.5 text-center"
                     >
                         Login
                     </button>
